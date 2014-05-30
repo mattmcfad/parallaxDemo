cb37222d7a07c01032528f84f8943a51cf2f9c24
Matt McFadyen <mmcfadyen91@gmail.com>
2014-05-30 01:42:27 -0400

    finaly getting somewhere wit parallaxJS :beers:

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
 
+//Image Scroll
+
 $('.img-holder').imageScroll({
     coverRatio: 0.5,
     speed: 0.2,
@@ -12,11 +14,44 @@ $('.img-holder-slow').imageScroll({
     extraHeight: 100,
 });
 
-  skrollr.init({
+
+//Skrollr signature
+skrollr.init({
     forceHeight: false,
     smoothScrolling: true,
-    
+});
 
-  });
+
+
+// jQuery(document).ready(function() {
+
+//   $('.parallax-layer').parallax({
+//     mouseport: $('.parallax-viewport')
+//   });
+// });
+
+
+
+// //parallax.js
+
+// // var scene = document.getElementById('parallax-viewport');
+// // var parallax = new Parallax(scene, {
+
+// // });
+
+$('#scene').parallax({
+    // calibrateX: false,
+    // calibrateY: true,
+    // invertX: false,
+    // invertY: true,
+    // limitX: false,
+    // limitY: 10,
+    // scalarX: 2,
+    // scalarY: 8,
+    // frictionX: 0.2,
+    // frictionY: 0.8,
+    // originX: 0.0,
+    // originY: 1.0
+});
 
 
